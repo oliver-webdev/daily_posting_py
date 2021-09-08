b024fe1be28910e9010070af1380a9ac9f86540f
junstice <62376782+junstice@users.noreply.github.com>
2021-09-08 13:20:55 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
 import os
+from datetime import datetime
+import pytz
 
 
 print(os.environ['MARKETSTACK_ACCESS_KEY'])
 print(os.environ['TISTORY_ACCESS_TOKEN'])
+
+now = datetime.utcnow()
+print(now)
+
+KST = pytz.timezone('Asia/Seoul')
+now_kst = now.astimezone(KST)
+print(now_kst.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
+
+
+print(now_kst.strftime("%Y"))
+print(now_kst.strftime("%m"))
+print(now_kst.strftime("%d"))
+
