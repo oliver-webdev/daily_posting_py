be181667fb79b83e4a99fffd1e2871f292b43d76
junstice <junstice@kakao.com>
2021-09-09 00:12:02 +0900

    날짜 비교 코드 샘플링

--- crawling-sample.py
+++ crawling-sample.py
@@ -12,31 +12,34 @@ if response.status_code == 200:
     soup = BeautifulSoup(html, 'html.parser')
     contents = soup.select('.article_list')
     for content in contents:
-        print(content.select_one('dt.tit > a').get_text())
-        print(content.find('a')['href'])
-        print(content.select_one('dd.desc > span.desctxt').get_text())
-        print(content.select_one('dd.desc > span.date').get_text())
 
         content_title = content.select_one('dt.tit > a').get_text()
         content_link = content.find('a')['href']
         content_desc = content.select_one('dd.desc > span.desctxt').get_text()
         content_date = content.select_one('dd.desc > span.date').get_text()
 
+        # 시간
         now = datetime.datetime.utcnow()
         KST = pytz.timezone('Asia/Seoul')
         now_kst = now.astimezone(KST)
-        # print(now_kst.strftime("%Y.%m.%d %H:%M"))
-
-        test_time = now_kst - datetime.timedelta(1)
-        # print(test_time.strftime("%Y.%m.%d %H:%M"))
 
         content_datetime = datetime.datetime.strptime(content_date, '%Y.%m.%d %H:%M')
-        print(content_datetime)
+        content_datetime_kst = content_datetime.astimezone(KST)
+        print(content_datetime_kst)
 
+        test_time = now_kst - datetime.timedelta(1)
         compare_time = test_time.replace(hour=8, minute=0, second=0, microsecond=0)
         print(compare_time)
 
-        # print(content_datetime > compare_time)
+        print(content_datetime_kst > compare_time)
+
+        if content_datetime_kst > compare_time:
+            print(content.select_one('dt.tit > a').get_text())
+            print(content.find('a')['href'])
+            print(content.select_one('dd.desc > span.desctxt').get_text())
+            print(content.select_one('dd.desc > span.date').get_text())
+        else:
+            break
 
 
 else:
